d14a6dd6b0ef6d75ed724e91b8e98f363f6bd060
Tech-A08 <tech-a08@tech-a08s-air.home>
2018-04-16 20:47:24 +0100

    can click the radio button

---
+++ selenium_class_walkthrough/selenium_class.rb
@@ -0,0 +1,47 @@
+require 'selenium-webdriver'
+
+class SeleniumQaToolForm
+
+  PRACTICE_FORM_URL = "http://toolsqa.com/automation-practice-form/"
+  FIRST_NAME_FIELD_NAME = "firstname"
+  LAST_NAME_FIELD_NAME = "lastname"
+
+  def initialize
+    @chrome_driver = Selenium::WebDriver.for :chrome
+  end
+
+  def vist_practice_form
+    @chrome_driver.get(PRACTICE_FORM_URL)
+  end
+
+  def input_firstname_field(text)
+    @chrome_driver.find_element(:name, FIRST_NAME_FIELD_NAME).send_keys(text)
+    sleep 3
+  end
+
+  def get_first_name_text
+    puts @chrome_driver.find_element(:name, FIRST_NAME_FIELD_NAME)['value']
+  end
+
+  def input_lastname_field(text)
+    @chrome_driver.find_element(:name, LAST_NAME_FIELD_NAME).send_keys(text)
+    sleep 5
+  end
+
+  def get_last_name_text
+    puts @chrome_driver.find_element(:name, LAST_NAME_FIELD_NAME)['value']
+  end
+
+  def input_choice_of_sex
+    element = @chrome_driver.find_element(:id, "sex-1").click
+    sleep 4
+  end
+end
+
+form = SeleniumQaToolForm.new
+form.vist_practice_form
+form.input_firstname_field("Deborah")
+form.get_first_name_text
+form.input_lastname_field("Ansah")
+form.get_last_name_text
+form.input_choice_of_sex
