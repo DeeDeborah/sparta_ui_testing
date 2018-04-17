c1dc17ef055d665e60241a398c82d503dc655375
Tech-A08 <tech-a08@tech-a08s-air.home>
2018-04-17 06:38:53 +0100

    continents list

--- selenium_class_walkthrough/selenium_class.rb
+++ selenium_class_walkthrough/selenium_class.rb
@@ -5,6 +5,7 @@ class SeleniumQaToolForm
   PRACTICE_FORM_URL = "http://toolsqa.com/automation-practice-form/"
   FIRST_NAME_FIELD_NAME = "firstname"
   LAST_NAME_FIELD_NAME = "lastname"
+  CONTINENT_LIST = 'continents'
 
   def initialize
     @chrome_driver = Selenium::WebDriver.for :chrome
@@ -34,26 +35,40 @@ class SeleniumQaToolForm
 
   def input_choice_of_sex
     @chrome_driver.find_element(:id, "sex-1").click
-    sleep 4
+    sleep 3
   end
 
   def click_years_of_exp
     @chrome_driver.find_element(:id, "exp-1").click
-    sleep 5
+    sleep 3
   end
 
   def input_date(text)
     @chrome_driver.find_element(:id, "datepicker").send_keys(text)
-    sleep 4
+    sleep 3
   end
 
   def select_profession
     @chrome_driver.find_element(:id, 'profession-1').click
-    sleep 4
+    sleep 3
   end
 
   def choose_photo(file)
     @chrome_driver.find_element(:id, 'photo').click
+  end
+
+  def choose_tool
+    @chrome_driver.find_element(:id, 'tool-2').click
+    sleep 3
+  end
+
+  def continents(continent)
+    con = @chrome_driver.find_element(:id, CONTINENT_LIST)
+		opt = dropdown.find_elements(:tag_name,"option")
+		opt.each do |opt|
+			opt.click if opt.text == continent
+		end
+		sleep 2
   end
 end
 
@@ -67,3 +82,5 @@ form.input_choice_of_sex
 form.click_years_of_exp
 form.input_date("12-03-18")
 form.select_profession
+form.choose_tool
+form.continents('Europe')
